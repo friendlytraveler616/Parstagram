8f750be83565e6764065511785d828634a6f9f79
friendlytraveler616 <53345715+friendlytraveler616@users.noreply.github.com>
2021-10-10 15:20:57 -0400

    Added project

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Joonho Jun on 10/10/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
